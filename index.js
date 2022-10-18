a5d67c7348be1309d230ba7e48aa88c29ef27974
jignesh upadhyay <jigneshupadhyay77@gmail.com>
2022-10-18 13:59:42 +0530

    complete

---
+++ index.js
@@ -0,0 +1,55 @@
+let display = document.getElementById("display");
+let button = Array.from(document.getElementsByClassName("button"));
+
+// console.log(button);
+
+// button click function target element
+
+button.map(button => {
+    button.addEventListener('click', (e) => {
+
+        // console.log("clicked");
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+
+        switch (e.target.innerText) {
+            case "c":
+                // clear inner text of display
+                display.innerText = "";
+                // break statement 
+                break;
+            case "←":
+                // if display has inner text 
+                if (display.innerText) {
+                    // remove last Character from display
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+
+            case "=":
+                try {
+                    // eval mathematics function  
+
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = " Error: ";
+                    // invalid mathematics expression then show Error
+                }
+                break;
+
+
+
+
+
+            default:
+                //  adding  innerText in display on 
+
+                display.innerText += e.target.innerText;
+            // break;
+        }
+
+
+
+
+    });
+});
